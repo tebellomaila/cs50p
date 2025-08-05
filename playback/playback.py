d29c1bdebcaa886b4c8c96420ba194dd0071a87e
tebellomaila <tebellomaila@outlook.com>
2025-08-05 22:45:02 +0200

    commit changes

--- playback/playback.py
+++
@@ -1,3 +0,0 @@
-text = input().strip()
-
-print(text.replace(" ", "..."))
---
+++ psets/0/einstein/einstein.py
@@ -0,0 +1,12 @@
+def main():
+    C = 300000000
+
+    m = int(input("m: "))
+
+    c = pow(C, 2)
+
+    print("E:", (m * c))
+
+
+if __name__ == "__main__":
+    main()
