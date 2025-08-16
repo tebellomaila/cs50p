c4d39b7c2da1a7d4d02bcc2905f1ac3b81d4e7a9
tebello <tebellomakhura@outlook.com>
2025-08-17 01:00:59 +0200

    modified files

--- 0/side_effects/hello.py
+++ 0/side_effects/hello.py
@@ -1,1 +1,21 @@
-print("hello, tebello")
+def main():
+    """ Prompt the user for their name and age and then greet the user"""
+    print("Hello!")
+    name = input("What's your name? ").strip() # Remove whitespaces
+
+    if not name:
+        print("Error: Please enter a valid name")
+        return
+
+    print(f"It is nice to meet, {name}")
+
+    """ Validate age input and print message """
+    try:
+        age = int(input("What's your age? "))
+        print(f"You will be {age + 1} years old after your birthday")
+    except ValueError:
+        print("Error: Please enter a numeric age")
+
+
+if __name__ == "__main__":
+    main()
--- 0/variables/guess.py
+++ 0/variables/guess.py
@@ -1,35 +1,42 @@
 SECRET = 30
 MAX_ATTEMPTS = 3
 
-def check_guess(n):
-    """ Validate user's guessed number to secret number """
+def get_hint(guess):
+    """ Return hint message based on guessed number """
     
-    if n > SECRET:
-        print("Too high")
-    elif n < SECRET:
-        print("Too low")
-    else:
-        print(f"You guessed number #{SECRET}\n")
-
-    return n == SECRET 
+    if guess > SECRET:
+        return "too high"
+    elif guess < SECRET:
+        return "too low"
+    
+    return "You lost"
+ 
 
 def main():
     """ Prompt user for input with the current attempt number """
     
     for attempt in range(1, MAX_ATTEMPTS + 1):
-        try:
-            user_input = input(f"Enter a number to guess (Attempt {attempt}/{MAX_ATTEMPTS}): ")
-            number = int(user_input) # integer conversion
+        while True:
+            try:
+                user_input = input(f"Enter a number to guess (Attempt {attempt}/{MAX_ATTEMPTS}): ")
+                
+                guess = int(user_input) # integer conversion 
+                break   # exit inner loop on valid input
 
-            if check_guess(number):
-                print("You won")
-                return
-            else:
-                print("Incorrect! Try again.")
-        except ValueError:
-            print(f"Error: {user_input} must be a number") # if conversion fails, raise an error message
+            except ValueError:
+                print(f"Error: expected a number. `{user_input}` is an invalid input.") # if conversion fails, raise an error message
+
+        if guess == SECRET:
+            print(f"You guessed the secret number. You won!")
+            return
+       
+        
+        hint = get_hint(guess)
+        prefix = f"The number you guessed was {hint}. Try again" if attempt < MAX_ATTEMPTS else ""
+        print(prefix)
+            
     
-    print("GAME OVER")
+    print(f"The secret number was {SECRET}")
 
 
 
