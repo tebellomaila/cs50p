ecc86ade931288320c71f92fa6d25b31048da3cc
tebello <tebellomakhura@outlook.com>
2025-08-20 12:01:49 +0200

    modified changes

--- 2/dictionaries/found_grades.py
+++ 2/dictionaries/found_grades.py
@@ -1,21 +1,19 @@
-""" Dummy data and dictionary of student results """
+""" A dictionary of student records to search """
 
-tebello = {"name": "tebello", "id": "214737600", "tests": [62,78,56], "exams": [74,48]}
-thabiso = {"name": "thabiso", "id": "213345809", "exams": [63,84]}
-mamello = {"name": "mamello", "id": "214534067", "tests": [78,82,65], "exams": [89,94]}
-seth = {"name": "seth", "id": "219600562", "tests": [84,67,79], "exams": [82,80]}
-tshepo = {"name": "tshepo", "id": "219600874", "tests": [], "exams":[]}
+STUDENTS = { 
+"tebello": {"id": "214737600", "tests": [62,78,56], "exams": [74,48]},
+"thabiso": {"id": "213345809", "exams": [63,84]},
+"mamello": {"id": "214534067", "tests": [78,82,65], "exams": [89,94]},
+"seth": {"id": "219600562", "tests": [84,67,79], "exams": [82,80]},
+"tshepo": {"id": "219600874", "tests": [], "exams":[]}
+}
 
-
-""" A list of student records to search """
-STUDENTS = [tshepo, tebello, mamello, seth, thabiso]
-
-""" Find grade results and return True if grade results exist otherwise False """
-def grades_found(student_name, student_list):
+""" Validate student exists and return True if student has grade results otherwise False """
+def grades_found(student_name, student_dict):
     # Search the student name in the list of dictionaries
-    for student in student_list:
-        if student["name"] == student_name:
-            return True if ("tests" in student and len(student["tests"]) > 0) or ("exams" in student and len(student["exams"]) >0) else False
+    if student_name in student_dict:
+        student = student_dict[student_name]
+        return True if ("tests" in student and len(student["tests"]) > 0) or ("exams" in student and len(student["exams"]) >0) else False
     return "Student not found"
 
 
