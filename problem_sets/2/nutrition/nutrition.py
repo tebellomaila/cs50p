5ab43b2287ddead371629e02a49b7152fa3f5c36
tebello <tebellomakhura@outlook.com>
2025-09-01 22:47:54 +0200

    modified changes

--- problem_sets/2/nutrition/nutrition.py
+++ problem_sets/2/nutrition/nutrition.py
@@ -1,37 +1,29 @@
+# FDA fruit calorie data per serving based on FDA poster
+fruit_calories = {
+    "apple": 130, "avocado": 50,"banana": 110,
+    "cantaloupe": 50, "grapefruit": 60, "grapes": 90,
+    "honeydew melon": 50, "kiwifruit": 90, "lemon": 15,
+    "lime": 20, "nectarine": 60, "orange": 80,
+    "peach": 60, "pear": 100, "pineapple": 50,
+    "plums": 70, "strawberries": 50, "sweet cherries": 100,
+    "tangerine": 50, "watermelon": 80
+    }
+
 def main():
     """ Check if fruit exists in calorie data and return its calories """
+    try:
+        # Prompt user for fruit name and normalise to lowercase
+        fruit_name = input("Item: ").strip().lower()
 
-    # FDA fruit calorie as per serving based on FDA poster
-    fruit_calories = {
-        "apple": 130,
-        "avocado": 50,
-        "banana": 110,
-        "cantaloupe": 50,
-        "grapefruit": 60,
-        "grapes": 90,
-        "honeydew melon": 50,
-        "kiwifruit": 90,
-        "lemon": 15,
-        "lime": 20,
-        "nectarine": 60,
-        "orange": 80,
-        "peach": 60,
-        "pear": 100,
-        "pineapple": 50,
-        "plums": 70,
-        "strawberries": 50,
-        "sweet cherries": 100,
-        "tangerine": 50,
-        "watermelon": 80
-    }
-    
-    # Prompt user for fruit input (case-insensitive)
-    fruit = input("Item: ").strip().lower()
-    
-
-    if fruit in fruit_calories:
-        # Output calories if fruit is found
-        print(f"Calories: {fruit_calories[fruit]}")
+        # Try to retrieve and print the calorie count
+        calories = fruit_calories[fruit_name]
+        print(f"Calories: {calories}")
+    except KeyError:
+        # Handle case where the input is not a recognised fruit
+        pass
+    except Exception as e:
+        # Handle any other unexpected errors
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
--- problem_sets/2/twttr/twttr.py
+++ problem_sets/2/twttr/twttr.py
@@ -1,21 +1,18 @@
-def remove_vowels(s):
+def remove_vowels(string):
+    """ Remove all vowels from input string """
+
+    # Use list comprehension to filter out vowels and then join the characters
     vowels = "aeiouAEIOU"
-
-    s_str = ""
-
-    for c in s:
-        if c not in vowels:
-            s_str += c
-    return s_str
-
+    return "".join([char for char in string if char not in vowels])
 
 def main():
-    str = input("Input: ").strip()
-    
-    output = remove_vowels(str)
-    
-    print("Output: ", end="")
-    print("".join(c for c in output))
+    # Prompt user for input and remove whitespaces
+    input_str = input("Input: ").strip()
+
+    # Process the input to remove vowels in the input string
+    output_str = remove_vowels(input_str)
+
+    print(f"Output: {output_str}")
 
 
 if __name__ == "__main__":
