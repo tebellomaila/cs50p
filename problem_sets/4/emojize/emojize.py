df72a03299589541198e89626ba99a2108b5696a
tebello <tebellomakhura@outlook.com>
2025-09-03 19:32:05 +0200

    commit modified changes

--- problem_sets/4/emojize/emojize.py
+++ problem_sets/4/emojize/emojize.py
@@ -1,10 +1,10 @@
-import emoji
+import emoji    # Import emoji loads the emoji library for converting text codes to actual emojis
 
 def main():
     """ Prompt the user for text and convert any emoji alias therein to their correspoding emoji """
     # Prompt user for input and convert emoji alias to actual emoji
     text = input("Input: ").strip()
-    print(f"Output: {emoji.emojize(text)}")
+    print("Output:", emoji.emojize(text, language="alias"))
 
 if __name__ == "__main__":
     main()
